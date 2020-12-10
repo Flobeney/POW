e35090c16f642cc77e94317669383dbdf8a8eb6a
Florent Beney <32325134+Flobeney@users.noreply.github.com>
2020-12-10 15:59:58 +0100

    Mise à jour de la base de donnée

--- BeneyCamaradeShooter/camaradeshooter.sql
+++ BeneyCamaradeShooter/camaradeshooter.sql
@@ -11,12 +11,21 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Listage de la structure de la table camaradeshooter. competences
+CREATE TABLE IF NOT EXISTS `competences` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domaine` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
 -- Listage de la structure de la table camaradeshooter. offices
 CREATE TABLE IF NOT EXISTS `offices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
@@ -29,6 +38,19 @@ CREATE TABLE IF NOT EXISTS `persons` (
   PRIMARY KEY (`id`),
   KEY `offices_id` (`offices_id`),
   CONSTRAINT `offices_id` FOREIGN KEY (`offices_id`) REFERENCES `offices` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la table camaradeshooter. person_has_competence
+CREATE TABLE IF NOT EXISTS `person_has_competence` (
+  `persons_id` int(11) NOT NULL,
+  `competences_id` int(11) NOT NULL,
+  PRIMARY KEY (`persons_id`,`competences_id`),
+  KEY `competences_id` (`competences_id`),
+  KEY `persons_id` (`persons_id`),
+  CONSTRAINT `competences_id` FOREIGN KEY (`competences_id`) REFERENCES `competences` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `persons_id` FOREIGN KEY (`persons_id`) REFERENCES `persons` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Les données exportées n'étaient pas sélectionnées.
